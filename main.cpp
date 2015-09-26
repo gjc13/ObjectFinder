223803b3376e397b9a75c1fa63afd081f5921930
gjc13 <amtcmp@126.com>
2015-09-26 12:19:46 +0800

    Image rebuild

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "common.h"
 #include "ProjectionDivider.h"
 #include "pointcloudUtilities.h"
+#include "ImageRebuild.h"
 #include <cstdio>
 
 using namespace std;
@@ -11,6 +12,7 @@ using namespace std;
 int main()
 {
     PointCloudPtr pointCloud(new pcl::PointCloud<pcl::PointXYZRGB>());
+    cv::Mat totalImage = cv::imread("/Users/gjc13/KinectData/rgb.png");
     if (pcl::io::loadPCDFile("/Users/gjc13/KinectData/pointCloud.pcd", *pointCloud) == -1)
     {
         PCL_ERROR("cannot load point cloud");
@@ -22,9 +24,13 @@ int main()
     char buffer[20];
     for (int i = 0; i < (int) dividedPointClouds.size(); i++)
     {
-        string prefix = "/Users/gjc13/KinectData/Divisions/divideCloud";
+        string pcdPrefix = "/Users/gjc13/KinectData/dividedSamples/divideCloud";
+        string lowResPrefix = "/Users/gjc13/KinectData/dividedSamples/lowRes";
+        string highResPrefix = "/Users/gjc13/KinectData/dividedSamples/highRes";
         sprintf(buffer, "%d", i);
-        pcl::io::savePCDFile(prefix + buffer + ".pcd", *dividedPointClouds[i], true);
+        pcl::io::savePCDFile(pcdPrefix + buffer + ".pcd", *dividedPointClouds[i], true);
+        cv::imwrite(lowResPrefix + buffer + ".png", get2DImageFromPointCloud(dividedPointClouds[i]));
+        cv::imwrite(highResPrefix + buffer + ".png", getHDImageFromPointCloud(dividedPointClouds[i], totalImage));
     }
     return 0;
 }
